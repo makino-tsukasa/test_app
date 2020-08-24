58c2b1ff50d9f821aa239ad14c57a417f91d1838
EC2 Default User <ec2-user@ip-172-31-40-209.ap-northeast-1.compute.internal>
2020-08-24 05:40:26 +0000

    change_for_utf8

--- db/migrate/20200818120827_create_posts.rb
+++ db/migrate/20200818120827_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
-    create_table :posts do |t|
+    create_table(:posts, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8') do |t|
       t.string :name
       t.text :content
 
